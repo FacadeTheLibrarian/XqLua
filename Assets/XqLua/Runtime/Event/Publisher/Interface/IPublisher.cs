10d91aaf5582738ada25721bba8ee5d60d0ac2fe
Yadori_Ouchi <yadoriouchi@gmail.com>
2026-09-11 15:13:39 +0900

    Publisherを二つの引数を持つActionに対応

--- Assets/XqLua/Runtime/Event/Publisher/Interface/IPublisher.cs
+++ Assets/XqLua/Runtime/Event/Publisher/Interface/IPublisher.cs
@@ -14,4 +14,19 @@ namespace XqLua {
         /// <returns>購読の解除を担当するDisposableSubscription</returns>
         public IDisposableSubscription Subscribe(Action<T> subscriber);
     }
+
+    /// <summary>
+    /// C#のeventを抽象化したインターフェース
+    /// 呼び出してほしいメソッドに引数を渡す
+    /// </summary>
+    /// <typeparam name="T">第一引数の型</typeparam>
+    /// <typeparam name="U">第二引数の型</typeparam>
+    public interface IPublisher<T, U> {
+        /// <summary>
+        /// 購読する
+        /// </summary>
+        /// <param name="subscriber">呼び出してほしいメソッド</param>
+        /// <returns>購読の解除を担当するDisposableSubscription</returns>
+        public IDisposableSubscription Subscribe(Action<T, U> subscriber);
+    }
 }
